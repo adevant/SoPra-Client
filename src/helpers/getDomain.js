4ac08cbc422dbb02217851566486bebfa80724c3
adevant <160859456+adevant@users.noreply.github.com>
2024-03-09 00:30:45 +0100

    Update getDomain.js url

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -2,7 +2,7 @@ import { isProduction } from "./isProduction"
 
 export const getDomain = () => {
   const prodUrl = "https://sopra-fs24-adevan-client.oa.r.appspot.com"
-  const devUrl = "https://sopra-fs24-adevan-server.oa.r.appspot.com"
+  const devUrl = "https://sopra-fs24-jukic-nedim-server.oa.r.appspot.com"
 
   return isProduction() ? prodUrl : devUrl
 }
